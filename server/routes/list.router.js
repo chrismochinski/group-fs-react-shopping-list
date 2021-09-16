123c37dd59b7079f5e31f3423d614cfb0dcb9f73
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-09-16 10:45:43 -0500

    Updated get router and get axios

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -7,6 +7,22 @@ const pool = require('../modules/pool.js');
 
 // GET ROUTE
 
+// Setup a GET route to get all the shoppingList from the database
+router.get('/', (req, res) => {
+    // When you fetch all things in these GET routes, strongly encourage ORDER BY
+    // so that things always come back in a consistent order 
+    const sqlText = `SELECT * FROM "shopping" ORDER BY "id" LIMIT 50;`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
 
 
 // POST ROUTE
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,42 @@ import './App.css';
 
 
 function App() {
+    let [shoppingList, setShoppingList] = useState([]);
+    let [itemName, setItemName] = useState('');
+    let [itemQuantity, setItemQuantity] = useState('');
+    let [itemUnit, setItemUnit] = useState('')
+
+
+  // This gets called with the component loads
+  // Basically $(ready)
+  useEffect(() => {
+    fetchShoppingList();
+  }, []);
+
+  const fetchShoppingList = () => {
+    axios({
+      method: 'GET',
+      url: '/list',
+    }).then((response) => {
+      // response.data is what we sent from the server
+      console.log(response.data);
+      setShoppingList(response.data);
+    }).catch((error) => {
+      console.log(error);
+      alert('Error making GET request.');
+    })
+  }
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
             </main>
+            <ul>
+            {props.list.map(shoppingList => 
+            (<li key={shoppingList.id}>{shoppingList.item} is from {shoppingList.quantity}</li>)
+            )}
+        </ul>
         </div>
     );
 }
